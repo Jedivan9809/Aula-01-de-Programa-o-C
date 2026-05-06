ba4312db3aa857f29713ea9174053a567c94f3e4
Jedivan9809 <90528299+Jedivan9809@users.noreply.github.com>
2026-05-06 19:13:53 -0300

    Add files via upload

---
+++ AULA_01.C
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main() {
+
+    int idade;
+    printf("Digite a idade: \n");
+    scanf("%d", &idade);
+    printf("A idade digitada foi: %d\n", idade);
+
+    return 0;
+}
